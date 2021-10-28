c45464647fac8e1e73389c3b44b8f9499c3aeed7
fyky <fuyu12dmb25ms@gmail.com>
2021-10-28 03:40:35 +0000

    [Add]homesページの整備・ルート設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :books
   devise_for :users
-  resources :users
+  resources :users, :only => [:show, :index ,:edit]
   root to: 'homes#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
