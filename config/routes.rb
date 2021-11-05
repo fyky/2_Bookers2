5917600ff040ce65d5813f9ed53053e294d6fb38
fyky <fuyu12dmb25ms@gmail.com>
2021-11-05 08:45:57 +0000

    [Add]必要ページ機能実装

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
   get 'about' => 'homes#about'
   devise_for :users
   resources :users
-  resources :books, only: [:new, :create, :index, :show, :destroy]
+  resources :books, only: [:new, :create, :index, :show, :destroy, :update, :edit]
 end
