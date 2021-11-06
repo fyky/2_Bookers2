2357c6539ef9d0a575d5cf74fcbcd0815b7de69c
fyky <fuyu12dmb25ms@gmail.com>
2021-11-06 15:53:31 +0000

    [Fix]エラー修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -42,11 +42,11 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
-      redirect_to book_path(book.id), notice: "You have updated book successfully."
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book), notice: "You have updated book successfully."
     else
-      render :new
+      render :edit
     end
   end
 
